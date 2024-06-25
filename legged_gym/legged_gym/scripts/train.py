bcd50b620e21a79a1c725f4c97e89d39a9d7af24
Jianhan (Jaime) Ma <61127883+Kingspenguin@users.noreply.github.com>
2024-06-25 19:34:12 +0800

    Update train.py

--- legged_gym/legged_gym/scripts/train.py
+++ legged_gym/legged_gym/scripts/train.py
@@ -40,7 +40,7 @@ import torch
 import wandb
 
 def train(args):
-    args.headless = False
+    args.headless = True
     log_pth = LEGGED_GYM_ROOT_DIR + "/logs/{}/".format(args.proj_name) + args.exptid
     try:
         os.makedirs(log_pth)
